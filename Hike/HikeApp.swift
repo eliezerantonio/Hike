589341829763b6a5d14663501fd8fb1667b61857
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-10-06 16:28:42 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Eliezer Antonio on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
